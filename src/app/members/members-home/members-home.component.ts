62c9f2adee3d37461b617d813dd6b3ccadc97d2d
Abdelrahman Alkot <abdelrahman.alkot2182004@gmail.com>
2025-03-29 12:38:23 +0200

    Project Intial Setup

---
+++ src/app/members/members-home/members-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-members-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './members-home.component.html',
+  styleUrl: './members-home.component.scss'
+})
+export class MembersHomeComponent {
+
+}

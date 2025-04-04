9244483c3be9bc626cc38351d8e4b176de078606
Abdelrahman Alkot <abdelrahman.alkot2182004@gmail.com>
2025-04-04 17:49:38 +0200

    Added Project Navigation to side bar

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,10 +1,11 @@
 //Angular
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 //Angular Materials
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MembersService } from './members.service';
 
 @Component({
     selector: 'app-members',
@@ -13,8 +14,9 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     styleUrl: './members.component.scss'
 })
 export class MembersComponent {
+  private membersService=inject(MembersService);
   isOpened: boolean = false;
-
+  projects=this.membersService.getProjectsByUserId(this.membersService.loggedInUser()?.userID || -1);
   toggleDrawer(): void {
     this.isOpened = !this.isOpened;
   }

cde76e9815967366f4313450e83108ab0f610361
Abdelrahman Alkot <abdelrahman.alkot2182004@gmail.com>
2025-04-05 22:48:41 +0200

    approval done

--- src/app/members/members-home/members-approval-table/members-approval-table.component.ts
+++ src/app/members/members-home/members-approval-table/members-approval-table.component.ts
@@ -44,15 +44,23 @@ export class MembersApprovalTableComponent {
     return this.membersService.getPendingApprovalRequest(task)?.status;
   }
 
+  currentTeamReviewing(task: Task): String | undefined{
+    return this.membersService.getPendingApprovalRequest(task)?.assigned.teamName;
+  }
+
   acceptTask(taskId: number){
     if(confirm("Are you sure you want to accept this Task? \nThis action is irreversable!")){
       this.membersService.acceptTask(taskId);
+      this.allReviewTasks=this.membersService.getReviewTasksForLoggedInUser();
+      this.applyFilter();
     };
   }
 
   rejectTask(taskId: number){
     if(confirm("Are you sure you want to reject this Task? \nThis action is irreversable!")){
       this.membersService.rejectTask(taskId);
+      this.allReviewTasks=this.membersService.getReviewTasksForLoggedInUser();
+      this.applyFilter();
     };
   }
 }

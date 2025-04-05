ce7a5079c543a15952fa46822e22bf343398e2e9
Abdelrahman Alkot <abdelrahman.alkot2182004@gmail.com>
2025-04-05 16:58:47 +0200

    Sidebar Styling

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,14 +1,14 @@
-
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatMenuModule } from '@angular/material/menu';
 import { MembersService } from './members.service';
 
 @Component({
   selector: 'app-members',
-  imports: [RouterOutlet, MatSidenavModule, MatButtonModule, MatMenuModule, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, MatSidenavModule, MatButtonModule,MatDividerModule, MatMenuModule, RouterLink, RouterLinkActive],
   templateUrl: './members.component.html',
   styleUrl: './members.component.scss'
 })

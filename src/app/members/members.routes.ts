3ef077fc1ef56d2683498408ce2ea725d30ec246
Mostafa Ayman <mostafaayman14g@gmail.con>
2025-04-05 19:48:33 -0700

    authentication for project done

--- src/app/members/members.routes.ts
+++ src/app/members/members.routes.ts
@@ -11,9 +11,33 @@ import { Project } from "../app.model";
  
 type ResolveFn<T> = (  route: ActivatedRouteSnapshot,  state: RouterStateSnapshot) => MaybeAsync<T | RedirectCommand>
 const ProjectResolver: ResolveFn<Project>=(route)=>{
-    return inject(MembersService).getProjectByProjectId(Number.parseInt(route.paramMap.get('projectId')!)) || new RedirectCommand(inject(Router).parseUrl('/404'));
+    return inject(MembersService).getProjectByProjectId(Number.parseInt(route.paramMap.get('projectID')!)) || new RedirectCommand(inject(Router).parseUrl('/404'));
 };
+  export const projectsAuthGuard: CanActivateFn = (route: ActivatedRouteSnapshot) => {
+    const service = inject(MembersService);
+    const router = inject(Router);
+  
+    const user = service.loggedInUser();
+    const projectIdFromUrl = Number(route.params['projectID']); 
 
+    if (!user || isNaN(projectIdFromUrl)) {
+      return router.navigateByUrl('/unauthorized');
+    }
+  
+  
+  
+    if (!user) {
+      return router.navigateByUrl('/unauthorized');
+    }
+  
+    const userProjects = service.getProjectsByUserId(user.userID);
+    const hasAccess = userProjects.some(project => project.projectID === projectIdFromUrl);
+    if (hasAccess) {
+      return true;
+    }
+  
+    return router.navigateByUrl('/unauthorized');
+  };
 export const membersRoutes: Routes=[
     {
         pathMatch:'full',
@@ -35,9 +59,10 @@ export const membersRoutes: Routes=[
                 component:ProjectsComponent,
             },
             {
-                path: ':projectId',
+                path: ':projectID',
                 component: ProjectDetailsComponent,
                 resolve: {Project: ProjectResolver},
+                canActivate: [projectsAuthGuard]
             },
         ]
     }
